95d86a3589100ade4a2f452bd9f9da7f9fbc6bb3
HenrikGharagyozyan1 <henrik.gharagyozyan1@gmail.com>
2026-07-21 12:38:28 +0400

    chore(project): initialize cpp-http-server project structure

---
+++ README.md
@@ -0,0 +1,2 @@
+# cpp-http-server
+A production-quality HTTP/1.1 server built from scratch in C++20.
---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() 
+{
+    std::cout << "Starting cpp-http-server..." << std::endl;
+
+    return 0;
+}
