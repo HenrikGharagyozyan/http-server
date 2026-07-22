295c441b8ec56631b88c5aa8eb2419f0469e5224
HenrikGharagyozyan1 <henrik.gharagyozyan1@gmail.com>
2026-07-22 13:22:53 +0400

    refactor(server): added enum class for status code and replace .cpp files to server folder

--- include/http/Response.hpp
+++ include/http/Response.hpp
@@ -6,10 +6,20 @@
 namespace http 
 {
 
+    enum class StatusCode
+    {
+        OK = 200,
+        BAD_REQUEST = 400,
+        NOT_FOUND = 404,
+        INTERNAL_SERVER_ERROR = 500
+    };
+
+    // Вспомогательная функция, чтобы не писать "OK" или "Not Found" руками
+    std::string get_status_message(StatusCode code);
+
     struct Response 
     {
-        int status_code{ 200 };
-        std::string status_message{ "OK" };
+        StatusCode status_code{ StatusCode::OK };
         
         std::unordered_map<std::string, std::string> headers;
         std::string body;
--- src/http/Response.cpp
+++ src/http/Response.cpp
@@ -1,9 +1,22 @@
 #include "http/Response.hpp"
 
 #include <sstream> // For std::ostringstream
+#include "Response.hpp"
 
 namespace http 
 {
+
+    std::string get_status_message(StatusCode code) 
+    {
+        switch (code) 
+        {
+            case StatusCode::OK:                    return "OK";
+            case StatusCode::BAD_REQUEST:           return "Bad Request";
+            case StatusCode::NOT_FOUND:             return "Not Found";
+            case StatusCode::INTERNAL_SERVER_ERROR: return "Internal Server Error";
+            default:                                return "Unknown";
+        }
+    }
 
     std::string Response::serialize() const 
     {
@@ -11,7 +24,8 @@ namespace http
         
         // 1. Status line
         // Format: HTTP/1.1 <Code> <Message>\r\n
-        oss << "HTTP/1.1 " << status_code << " " << status_message << "\r\n";
+        oss << "HTTP/1.1 " << static_cast<int>(status_code) << " " 
+            << get_status_message(status_code) << "\r\n";
         
         // 2. Headers
         // Format: <Key>: <Value>\r\n
